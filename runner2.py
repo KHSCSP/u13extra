11a1dd2cc942fd758939a76e7735ce31c3f4b5ff
heyscottn <heyscottn@gmail.com>
2026-02-12 13:49:22 -0600

    mm

--- runner2.py
+++ runner2.py
@@ -5,7 +5,7 @@
     Loop through and print the content, 
     but add a checkmark (✓) if is_verified is True.
 
-    Use sorted() with a lambda (or a simple loop if they aren't there yet)
+    Use sorted() with a lambda (or a 'best so far' loop)
     to find the post with the most likes.
 
     Ask the user for a username, then display all "posts" by that specific user.
